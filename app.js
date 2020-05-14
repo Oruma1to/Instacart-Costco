edc31d3a606eb6aead4a8372ddebb8c2ced40588
joshua aponte <orumaito@joshuas-MacBook-Pro.local>
2020-05-14 10:39:45 -0400

    added Server.js and app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const routes = require('./routes');
+
+
+//This is my server App logic
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+app.use('/api', routes);
+
+module.exports = app
+
