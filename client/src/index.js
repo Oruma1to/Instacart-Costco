68a9dab4d60bc639722b2f08616f8e49f009ff1a
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-15 11:07:02 -0400

    Added StoreFront component / refactor the LandingPage component with Redirect / App file to .jsx / index.js change the import file for App

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+ <Router>
+  <App />
+ </Router>,
+ document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
