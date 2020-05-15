b5614844cff12a897cf2ca0cca32b424fd92f748
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2020-05-15 18:03:49 -0400

    Costco log and sub text added

--- client/src/components/StoreFront/StoreHeader.jsx
+++ client/src/components/StoreFront/StoreHeader.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import './StoreHeader.css'
 import StoreFrontNavBar from './StoreFrontNavBar'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
 
 export default class StoreHeader extends Component {
   render() {
     return (
       <div className='apple-header'>
         <StoreFrontNavBar />
+
+        <div className="circle-logo"><img className="costco-logo" src={require("../../images/costco-logo2.png")} /></div>
+        
+        <div className="costco-title">Costco</div>
+        
+        <div className="view-more-links"><Link className="aboveSearch">View pricing policy</Link><span className="dot">.</span><Link className="aboveSearch">View more</Link></div>
+
         <SearchBar />
       </div>
     )
