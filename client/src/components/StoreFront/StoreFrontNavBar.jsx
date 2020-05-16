01e9d601f4f18d29878312da06112a5445a65479
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-16 17:29:24 -0400

    Fix the landingpage login by editing the StoreFrontNavBar.css

--- client/src/components/StoreFront/StoreFrontNavBar.jsx
+++ client/src/components/StoreFront/StoreFrontNavBar.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 import './StoreFrontNavBar.css'
 
 export default class StoreFrontNavBar extends Component {
-  render() {
-    return (
-      <>
-        <nav className='header-navBar'>
-          <div className='left-side'>
-            <img className="instacart-logo" src={require("../../images/instacart-logo-white@3x.png")} alt="instacart" />
-            <Link src='#'>Stores</Link>
-          </div>
+ render() {
+  return (
+   <>
+    <nav className='header-navBar'>
+     <div className='left-side'>
+      <img className="instacart-logo" src={require("../../images/instacart-logo-white@3x.png")} alt="instacart" />
+      <Link className="storeFrontNavBar-link" src='#'>Stores</Link>
+     </div>
 
-          <div className='right-side'>
-            <Link src='#'>Account</Link>
-            <Link src='#'>Help</Link>
-            <img src={require("../../images/cart.png")} alt="cart" className="cart-logo" />
-          </div>
-        </nav>
-      </>
-    )
-  }
+     <div className='right-side'>
+      <Link className="storeFrontNavBar-link" src='#'>Account</Link>
+      <Link className="storeFrontNavBar-link" src='#'>Help</Link>
+      <img src={require("../../images/cart.png")} alt="cart" className="cart-logo" />
+     </div>
+    </nav>
+   </>
+  )
+ }
 }
