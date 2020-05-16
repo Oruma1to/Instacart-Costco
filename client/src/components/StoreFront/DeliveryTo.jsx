d5e1b78a8d6b08a8dd08e166295969f6f4d93055
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-16 18:13:59 -0400

    put some alt in some images tag in Delivery.jsx and StoreHeader.jsx

--- client/src/components/StoreFront/DeliveryTo.jsx
+++ client/src/components/StoreFront/DeliveryTo.jsx
@@ -3,20 +3,20 @@ import React from "react";
 import "./DeliveryTo.css";
 
 export const DeliveryTo = () => {
-  return (
-    <div className="delivery-to">
-      <p>Delivery to</p>
+ return (
+  <div className="delivery-to">
+   <p>Delivery to</p>
 
-      <div className="drop-down">
-        10014 <i className="arrow-down"></i>
-      </div>
+   <div className="drop-down">
+    10014 <i className="arrow-down"></i>
+   </div>
 
-      <span className="delivey-date">Apr 23 - Apr 24</span>
-      <img
-        className="info-icon"
-        src={require("../../images/info-icon.png")}
-        atl="information icon"
-      />
-    </div>
-  );
+   <span className="delivey-date">Apr 23 - Apr 24</span>
+   <img
+    className="info-icon"
+    src={require("../../images/info-icon.png")}
+    alt="information icon"
+   />
+  </div>
+ );
 };
