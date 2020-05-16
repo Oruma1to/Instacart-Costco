80b76fa0aa19d3240e5ac111ee97adee03b1a394
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-16 18:06:08 -0400

    gave a more detail classname for the img tag of instacart-detail-image to sign-in-instacart-detail-image and change the margin bottom to get closer and make it look alike as the ux design

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -5,118 +5,118 @@ import LandingHeader from './LandingHeader'
 
 
 class SignIn extends Component {
-  constructor(props) {
-    super(props)
+ constructor(props) {
+  super(props)
 
-    this.state = {
-      email: '',
-      password: '',
-      isError: false,
-      errorMsg: ''
-    }
+  this.state = {
+   email: '',
+   password: '',
+   isError: false,
+   errorMsg: ''
   }
+ }
 
-  handleChange = event => {
+ handleChange = event => {
+  this.setState({
+   [event.target.name]: event.target.value,
+   isError: false,
+   errorMsg: ''
+  })
+ }
+
+ onSignIn = event => {
+  event.preventDefault()
+  console.log(this.props)
+  const { history, setUser } = this.props
+  console.log(setUser)
+  signIn(this.state)
+   .then(res => {
+    setUser(res.user)
+   })
+   .then(() => history.push('/costco'))
+   .catch(error => {
+    console.error(error)
     this.setState({
-      [event.target.name]: event.target.value,
-      isError: false,
-      errorMsg: ''
+     isError: true,
+     errorMsg: 'Invalid Credentials',
+     email: '',
+     password: ''
     })
+   })
+ }
+
+ renderError = () => {
+  console.log(this.state.email)
+  const toggleForm = this.state.isError ? 'danger' : ''
+  if (this.state.isError) {
+   return (
+    <button type="submit" className={toggleForm}>
+     {this.state.errorMsg}
+    </button>
+   )
+  } else {
+   return <button className="logIn" type="submit">Log In</button>
   }
+ }
 
-  onSignIn = event => {
-    event.preventDefault()
-    console.log(this.props)
-    const { history, setUser } = this.props
-    console.log(setUser)
-    signIn(this.state)
-      .then(res => {
-        setUser(res.user)
-      })
-      .then(() => history.push('/costco'))
-      .catch(error => {
-        console.error(error)
-        this.setState({
-          isError: true,
-          errorMsg: 'Invalid Credentials',
-          email: '',
-          password: ''
-        })
-      })
-  }
+ render() {
+  const { email, password } = this.state
+  return (
 
-  renderError = () => {
-    console.log(this.state.email)
-    const toggleForm = this.state.isError ? 'danger' : ''
-    if (this.state.isError) {
-      return (
-        <button type="submit" className={toggleForm}>
-          {this.state.errorMsg}
-        </button>
-      )
-    } else {
-      return <button className="logIn" type="submit">Log In</button>
-    }
-  }
+   <>
+    <LandingHeader />
 
-  render() {
-    const { email, password } = this.state
-    return (
+    <div className="form-container">
+     <img className="sign-in-instacart-detail-image" alt="instacart-logo"
+      src={require('../images/instacartlogo@3x.png')} />
 
-      <>
-        <LandingHeader />
+     {/* <div className="instacart-detail-image"></div> */}
+     <div className="welcome">Welcome back</div>
+     <div className="login-prompt">Log in with your email and password</div>
+     <form onSubmit={this.onSignIn}>
+      <div className="emailInput">
+       <input
+        required
+        type="text"
+        name="email"
+        value={email}
+        id="emailInput"
+        placeholder="Email address"
+        onChange={this.handleChange}
+       />
+      </div>
+      <div className="passwordInput">
+       <input
+        required
+        type="text"
+        name="password"
+        value={password}
+        placeholder="Password (min 6 characters)"
+        onChange={this.handleChange}
+       />
+      </div>
+      {this.renderError()}
 
-        <div className="form-container">
-          <img className="instacart-detail-image" alt="instacart-logo"
-            src={require('../images/instacartlogo@3x.png')} />
+      <div className="separator">or</div>
 
-          {/* <div className="instacart-detail-image"></div> */}
-          <div className="welcome">Welcome back</div>
-          <div className="login-prompt">Log in with your email and password</div>
-          <form onSubmit={this.onSignIn}>
-            <div className="emailInput">
-              <input
-                required
-                type="text"
-                name="email"
-                value={email}
-                id="emailInput"
-                placeholder="Email address"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="passwordInput">
-              <input
-                required
-                type="text"
-                name="password"
-                value={password}
-                placeholder="Password (min 6 characters)"
-                onChange={this.handleChange}
-              />
-            </div>
-            {this.renderError()}
-
-            <div className="separator">or</div>
-
-            <div className="orLoginInfo">
-              <div>
-                <button className="facebookButton">Continue with Facebook</button>
-              </div>
-              <div>
-                <button className="googleButton">Continue with Google</button>
-              </div>
-              <div className="alreadyLogin">
-                <h4>Already have an account? <span style={{ color: "green" }}> Log In </span></h4>
-              </div>
-            </div>
+      <div className="orLoginInfo">
+       <div>
+        <button className="facebookButton">Continue with Facebook</button>
+       </div>
+       <div>
+        <button className="googleButton">Continue with Google</button>
+       </div>
+       <div className="alreadyLogin">
+        <h4>Already have an account? <span style={{ color: "green" }}> Log In </span></h4>
+       </div>
+      </div>
 
 
-          </form>
-        </div >
-      </>
-    )
-  }
+     </form>
+    </div >
+   </>
+  )
+ }
 
 
 }
