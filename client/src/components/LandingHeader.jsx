3ab8160307b145751e238365e4cd498959a83ff0
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-16 21:15:57 -0400

    Added some changes to the landing header so the shopper div is center and the login-link-container is to the right with posiotn absolute

--- client/src/components/LandingHeader.jsx
+++ client/src/components/LandingHeader.jsx
@@ -3,22 +3,22 @@ import { NavLink } from "react-router-dom";
 import "./LandingHeader.css";
 
 const LandingHeader = () => {
-  return (
-    <nav className="nav-main">
-      <div className="shopper">
-        <p>Want to earn extra money?</p>
-        <button className="shopper-btn">Become a Shopper</button>
-      </div>
+ return (
+  <nav className="nav-main">
+   <div className="shopper">
+    <p>Want to earn extra money?</p>
+    <button className="shopper-btn">Become a Shopper</button>
+   </div>
 
-      <div className="outer-btn">
-        <button className="login-link-container">
-          <NavLink className="login-link" to="/sign-in">
-            Log in
+   {/* <div className="outer-btn"> */}
+   <button className="login-link-container">
+    <NavLink className="login-link" to="/sign-in">
+     Log in
           </NavLink>
-        </button>
-      </div>
-    </nav>
-  );
+   </button>
+   {/* </div> */}
+  </nav>
+ );
 };
 
 export default LandingHeader;
