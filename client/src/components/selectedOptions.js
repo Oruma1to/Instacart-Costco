e24224b79495ec224784c25ce1d8e1072e89ec14
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-18 17:39:25 -0400

    here is my file where I keep my arrays for the selected deparment I also added a function that will return the array list for the rigth category

--- client/src/components/selectedOptions.js
+++ client/src/components/selectedOptions.js
@@ -7,25 +7,65 @@ export const departments = [
 ];
 
 //Category list
+// export const categories = [
+//  { value: 'Prime Beef', label: 'Prime Beef', name: "category" },
+//  { value: 'Pork & Lamb', label: 'Pork & Lamb', name: "category" },
+//  { value: 'Seafood', label: 'Seafood', name: "category" },
+//  { value: 'Poultry', label: 'Poultry', name: "category" },
+//  { value: 'Meat', label: 'Meat' },
+//  { value: 'Fruit & Vegetable Trays', label: 'Fruit & Vegetable Trays', name: "category" },
+//  { value: 'Fruit', label: 'Fruit', name: "category" },
+//  { value: 'Vegetables', label: 'Vegetables', name: "category" },
+//  { value: 'Salad Mix', label: 'Salad Mix', name: "category" },
+//  { value: 'Meat & Cheese Trays', label: 'Meat & Cheese Trays', name: "category" },
+//  { value: 'Prepared Meals', label: 'Prepared Meals', name: "category" },
+//  { value: 'Prepared Soups & Salad', label: 'Prepared Soups & Salad', name: "category" },
+//  { value: 'Dips & Spreads', label: 'Dips & Spreads', name: "category" },
+//  { value: 'Bread', label: 'Bread', name: "category" },
+//  { value: 'Buns & Rolls', label: 'Buns & Rolls', name: "category" },
+//  { value: 'Cookies', label: 'Cookies', name: "category" },
+//  { value: 'Pies & Cakes', label: 'Pies & Cakes', name: "category" }
 
-export const categories = [
- { value: 'Prime Beef', label: 'Prime Beef', name: "category" },
- { value: 'Pork & Lamb', label: 'Pork & Lamb', name: "category" },
- { value: 'Seafood', label: 'Seafood', name: "category" },
- { value: 'Poultry', label: 'Poultry', name: "category" },
- { value: 'Meat', label: 'Meat' },
- { value: 'Fruit & Vegetable Trays', label: 'Fruit & Vegetable Trays', name: "category" },
- { value: 'Fruit', label: 'Fruit', name: "category" },
- { value: 'Vegetables', label: 'Vegetables', name: "category" },
- { value: 'Salad Mix', label: 'Salad Mix', name: "category" },
- { value: 'Meat & Cheese Trays', label: 'Meat & Cheese Trays', name: "category" },
- { value: 'Prepared Meals', label: 'Prepared Meals', name: "category" },
- { value: 'Prepared Soups & Salad', label: 'Prepared Soups & Salad', name: "category" },
- { value: 'Dips & Spreads', label: 'Dips & Spreads', name: "category" },
- { value: 'Bread', label: 'Bread', name: "category" },
- { value: 'Buns & Rolls', label: 'Buns & Rolls', name: "category" },
- { value: 'Cookies', label: 'Cookies', name: "category" },
- { value: 'Pies & Cakes', label: 'Pies & Cakes', name: "category" }
+// ];
 
-];
+export const checker = (value) => {
+ let category;
+ switch (value) {
+  case 'Meat & Seafood':
+   category = [
+    { value: 'Prime Beef', label: 'Prime Beef', name: "category" },
+    { value: 'Pork & Lamb', label: 'Pork & Lamb', name: "category" },
+    { value: 'Seafood', label: 'Seafood', name: "category" },
+    { value: 'Poultry', label: 'Poultry', name: "category" },
+    { value: 'Meat', label: 'Meat' },
+   ]
+   break;
+  case 'Produce':
+   category = [
+    { value: 'Fruit & Vegetable Trays', label: 'Fruit & Vegetable Trays', name: "category" },
+    { value: 'Fruit', label: 'Fruit', name: "category" },
+    { value: 'Vegetables', label: 'Vegetables', name: "category" },
+    { value: 'Salad Mix', label: 'Salad Mix', name: "category" },
+   ]
+   break;
+  case 'Deli':
+   category = [
+    { value: 'Meat & Cheese Trays', label: 'Meat & Cheese Trays', name: "category" },
+    { value: 'Prepared Meals', label: 'Prepared Meals', name: "category" },
+    { value: 'Prepared Soups & Salad', label: 'Prepared Soups & Salad', name: "category" },
+    { value: 'Dips & Spreads', label: 'Dips & Spreads', name: "category" },
+   ]
+   break;
+  case 'Bakery & Dessert':
+   category = [
+    { value: 'Bread', label: 'Bread', name: "category" },
+    { value: 'Buns & Rolls', label: 'Buns & Rolls', name: "category" },
+    { value: 'Cookies', label: 'Cookies', name: "category" },
+    { value: 'Pies & Cakes', label: 'Pies & Cakes', name: "category" }
+   ]
+  default:
+   break;
+ }
 
+ return category
+}
