bf753a407fa099c34cc7da5cf001bc64b6765bc1
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-18 12:36:07 -0400

    Here i am calling ProductCreate.jsx to create the page of how its going to look

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -3,13 +3,15 @@ import LandingHeader from "./LandingHeader";
 import "./LandingHeader.css";
 import { Redirect } from "react-router-dom";
 import './LandingPage.css'
+import ProductCreate from "./ProductCreate";
 
 export default class LandingPage extends Component {
  render() {
   return (
    <>
     <LandingHeader />
-    <Redirect to="sign-up" />
+    {/* <Redirect to="sign-up" /> */}
+    <ProductCreate />
    </>
   );
  }
