f8129d5fe19dd5bd9c4734813e2b97a836519a5d
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-17 21:08:01 -0400

    Was able to create a dropdown button and then made the DropdownMenu dynamice to be reuse

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import LandingHeader from "./LandingHeader";
+import DropdownMenu from "./shared/DropdownMenu"
 import "./LandingHeader.css";
 import { Redirect } from "react-router-dom";
 import './LandingPage.css'
 
 export default class LandingPage extends Component {
-  render() {
+ render() {
   return (
-  <>
+   <>
     <LandingHeader />
-    <Redirect to="sign-up" />
-  </>
+    <DropdownMenu titleMenu={"Account"} />
+    <DropdownMenu titleMenu={"Stores"} />
+
+    {/* <Redirect to="sign-up" /> */}
+   </>
   );
-  }
+ }
 }
---
+++ client/src/components/shared/DropdownMenu.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import "./DropdownMenu.css"
+
+export default class DropdownMenu extends Component {
+ render() {
+  let choosenMenu = [];
+  console.log("In DropDown Menu ", this.props)
+  if (this.props.titleMenu === "Account") {
+   console.log("Yes it matches")
+   choosenMenu = [
+    <Link key="1" className="dropdown-links" to="#">Add new Product</Link>,
+    <Link key="2" className="dropdown-links" to="#">Edit Product</Link>,
+    <Link key="3" className="dropdown-links" to="#">Delete Product</Link>,
+    <Link key="4" className="dropdown-links" to="#">Sign Out</Link>
+   ]
+  } else if (this.props.titleMenu === "Stores") {
+   choosenMenu = [
+    <Link key="1" className="dropdown-links" to="#">Queen</Link>,
+    <Link key="2" className="dropdown-links" to="#">Manhattan</Link>,
+    <Link key="3" className="dropdown-links" to="#">Yonkers</Link>
+   ]
+  }
+  console.log("choosenMenu", choosenMenu)
+  return (
+   <div className="dropDownMenu" >
+    <button className="dropbtn">{this.props.titleMenu}
+     <i className="dropdown-arrow-down"></i>
+    </button>
+    <div className="dropdown-content">
+     {choosenMenu.map(tag => tag)}
+    </div>
+   </div>
+  )
+ }
+}
+
+//Testing purposes
+   // choosenMenu = [
+   //  {
+   //   name: "Add new Product",
+   //   where: "#"
+   //  },
+   //  {
+   //   name: "Edit Product",
+   //   where: "#"
+   //  },
+   //  {
+   //   name: "Delete Product",
+   //   where: "#"
+   //  },
+   //  {
+   //   name: "Sign out",
+   //   where: "#"
+   //  },
+   // ]
