79c2d799a0809c972304842f3e3789a2bfed81d3
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-18 11:08:22 -0400

    added framework of side nav

--- client/src/components/StoreFront/Departments.jsx
+++ client/src/components/StoreFront/Departments.jsx
@@ -5,7 +5,77 @@ export default class Departments extends Component {
     return (
       <div>
         <h1>Departments</h1>
-        
+        <nav id="side-menu">
+
+          <ul class="parent-menu">
+
+            <li><a href="#">Meat & Seafood</a>
+
+              <ul>
+
+                <li><a href="#">Prime Beef</a></li>
+
+                <li><a href="#">Pork & Lamb</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+              </ul>
+
+            </li>
+
+            <li><a href="#">header</a>
+
+              <ul>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+              </ul></li>
+
+            <li><a href="#">header</a>
+
+              <ul>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+              </ul></li>
+
+            <li><a href="#">header</a>
+
+              <ul>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+                <li><a href="#">item</a></li>
+
+              </ul>
+
+            </li>
+          </ul>
+        </nav>
       </div>
     )
   }
