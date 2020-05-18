9a468cbec9eda92f757c11aad073ffc122219509
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-18 12:36:29 -0400

    Create ProductCreate files

---
+++ client/src/components/ProductCreate.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./ProductCreate.css"
+
+export default class ProductCreate extends Component {
+ render() {
+  return (
+   <div>
+
+   </div>
+  )
+ }
+}
