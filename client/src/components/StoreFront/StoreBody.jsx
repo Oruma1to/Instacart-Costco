45efa3df9e4794097c6d0702ce189aa57fbdda40
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-18 11:03:10 -0400

    added department branch

--- client/src/components/StoreFront/StoreBody.jsx
+++ client/src/components/StoreFront/StoreBody.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { DeliveryTo } from "./DeliveryTo";
+import Department from "./Departments"
 
 export const StoreBody = () => {
   return (
     <div>
       <DeliveryTo />
+      <Department />
     </div>
   );
 };
