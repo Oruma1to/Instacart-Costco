a617612b58c4a586af70ec25df416c9e17d79740
Abraham E. Tavarez <abetavarez@icloud.com>
2020-05-19 11:20:14 -0400

    Created ProductDetail component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,77 @@
-import React, { Component } from 'react'
-import "./App.css"
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { verifyUser } from './services/user'
-import SignUp from './components/SignUp'
-import LandingPage from "./components/LandingPage"
-import SignIn from './components/SignIn'
-import StoreFront from './components/StoreFront/StoreFront'
-import SignOut from './components/SingOut'
-
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { verifyUser } from "./services/user";
+import SignUp from "./components/SignUp";
+import LandingPage from "./components/LandingPage";
+import SignIn from "./components/SignIn";
+import StoreFront from "./components/StoreFront/StoreFront";
+import SignOut from "./components/SingOut";
+import ProductDetail from "./components/ProductDetail";
 
 export default class App extends Component {
- constructor() {
-  super()
-  this.state = {
-   user: null
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
   }
- }
 
-
- async componentDidMount() {
-  const user = await verifyUser()
-  if (user) {
-   this.setState(user)
+  async componentDidMount() {
+    const user = await verifyUser();
+    if (user) {
+      this.setState(user);
+    }
   }
- }
 
- setUser = user => this.setState({ user })
+  setUser = (user) => this.setState({ user });
 
- clearUser = () => this.setState({ user: null })
+  clearUser = () => this.setState({ user: null });
 
- render() {
-  const { setUser, clearUser } = this
-  const { user } = this.state
-  console.log("In App page", user);
+  render() {
+    const { setUser, clearUser } = this;
+    const { user } = this.state;
+    console.log("In App page", user);
 
-  return (
-   <div className="app">
-    <Switch>
-     <Route exact path="/" render={() => <LandingPage />} />
-     <Route exact path="/sign-up" render={() => <SignUp />} />
-     <Route exact path="/sign-in" render={routerProps => <SignIn setUser={setUser} history={routerProps.history} />} />
-     <Route exact path="/sign-out" render={props => <SignOut user={user} clearUser={clearUser} history={props.history} />} />
-     {/* <Route exact path="/costco" render={() => user ? <StoreFront user={user} /> : <Redirect to="/sign-up" />} /> */}
-     <Route exact path="/costco" render={() => <StoreFront user={user} />} />
-    </Switch>
-   </div>
-  )
- }
+    return (
+      <div className="app">
+        <Switch>
+          <Route exact path="/" render={() => <LandingPage />} />
+          <Route exact path="/sign-up" render={() => <SignUp />} />
+          <Route
+            exact
+            path="/sign-in"
+            render={(routerProps) => (
+              <SignIn setUser={setUser} history={routerProps.history} />
+            )}
+          />
+          <Route
+            exact
+            path="/sign-out"
+            render={(props) => (
+              <SignOut
+                user={user}
+                clearUser={clearUser}
+                history={props.history}
+              />
+            )}
+          />
+          {/* <Route exact path="/costco" render={() => user ? <StoreFront user={user} /> : <Redirect to="/sign-up" />} /> */}
+          <Route
+            exact
+            path="/costco"
+            render={() => <StoreFront user={user} />}
+          />
+          <Route
+            exact
+            path="/costco/:id"
+            render={(props) => (
+              <ProductDetail history={props.history} user={user} />
+            )}
+          />
+          } />
+        </Switch>
+      </div>
+    );
+  }
 }
---
+++ client/src/components/ProductDetail.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+import "./ProductDetail.css";
+import { getProduct } from "../services/product";
+import { Link } from "react-router-dom";
+
+class ProductDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: {
+        name: "",
+        brand: "",
+        department: "",
+        category: "",
+        imageURL: "",
+        price: 0,
+        quantity: 0,
+        size: "",
+      },
+    };
+  }
+
+  async componentDidMount() {
+    let { id } = this.props.match.params;
+    const product = await getProduct(id);
+    this.setState({ product });
+  }
+
+  render() {
+    const { product } = this.state;
+    return (
+      <div>
+        <div>
+          <img className="" src={product.imageURL} alt={product.name} />
+        </div>
+        <div>
+          <div className="">{product.name}</div>
+          <div className="">{product.brand}</div>
+          <div className="">{product.department}</div>
+          <div className="">{product.category}</div>
+          <div className="">${product.price}</div>
+          <div className="">{product.quantity}</div>
+          <div className="">{product.size}</div>
+        </div>
+        {/* <div>
+          <button className="edit-btn"><Link>EDIT</Link></button>
+          <button className="delete-btn">DELETE</button>
+        </div> */}
+      </div>
+    );
+  }
+}
+
+export default ProductDetail;
