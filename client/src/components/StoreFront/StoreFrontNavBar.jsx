85d8f5702b330f6e1aa2e7c69acb9a7573b361b3
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-19 12:21:59 -0400

    cleaned up font size of acct and store

--- client/src/components/StoreFront/StoreFrontNavBar.jsx
+++ client/src/components/StoreFront/StoreFrontNavBar.jsx
@@ -4,25 +4,25 @@ import './StoreFrontNavBar.css'
 import DropdownMenu from '../shared/DropdownMenu'
 
 export default class StoreFrontNavBar extends Component {
- render() {
-  console.log("In Store Front Nav Bar ", this.props.user)
-  return (
-   <>
-    <nav className='header-navBar'>
-     <div className='left-side'>
-      <img className="instacart-logo" src={require("../../images/instacart-logo-white@3x.png")} alt="instacart" />
-      {/* <Link className="storeFrontNavBar-link" src='#'>Stores</Link> */}
-      <DropdownMenu titleMenu={"Stores"} user={this.props.user} />
-     </div>
+  render() {
+    console.log("In Store Front Nav Bar ", this.props.user)
+    return (
+      <>
+        <nav className='header-navBar'>
+          <div className='left-side'>
+            <img className="instacart-logo" src={require("../../images/instacart-logo-white@3x.png")} alt="instacart" />
+            {/* <Link className="storeFrontNavBar-link" src='#'>Stores</Link> */}
+            <DropdownMenu titleMenu={"Stores"} user={this.props.user} />
+          </div>
 
-     <div className='right-side'>
-      {/* <Link className="storeFrontNavBar-link" src='#'>Account</Link> */}
-      <DropdownMenu titleMenu={"Account"} user={this.props.user} />
-      <Link className="storeFrontNavBar-link" to='#'>Help</Link>
-      <img src={require("../../images/cart.png")} alt="cart" className="cart-logo" />
-     </div>
-    </nav>
-   </>
-  )
- }
+          <div className='right-side'>
+            {/* <Link className="storeFrontNavBar-link" src='#'>Account</Link> */}
+            <DropdownMenu titleMenu={"Account"} user={this.props.user} />
+            <Link className="storeFrontNavBar-link" to='#'>Help</Link>
+            <img src={require("../../images/cart.png")} alt="cart" className="cart-logo" />
+          </div>
+        </nav>
+      </>
+    )
+  }
 }
