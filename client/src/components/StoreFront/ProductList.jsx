975b4d889c4e4e321675abe2f82eb40cccf1e6e9
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-18 22:27:24 -0400

    Able to fix the productList width of the 'all-carousels' class

--- client/src/components/StoreFront/ProductList.jsx
+++ client/src/components/StoreFront/ProductList.jsx
@@ -6,57 +6,57 @@ import "./ProductList.css";
 import { Link } from 'react-router-dom'
 
 class ProductList extends Component {
-  constructor() {
-    super();
+ constructor() {
+  super();
 
-    this.state = {
-      products: [],
-    };
-  }
+  this.state = {
+   products: [],
+  };
+ }
 
-  async componentDidMount() {
-    const products = await getProducts();
-    this.setState({ products });
-  }
+ async componentDidMount() {
+  const products = await getProducts();
+  this.setState({ products });
+ }
 
-  render() {
-    console.log("all products-->", this.state.products);
-    const PRODUCTS = this.state.products
-      .slice(0, 4)
-      .map((product, index) => (
-        <Product
-          key={index}
-          _id={product._id}
-          imageURL={product.imageURL}
-          brand={product.brand}
-          name={product.name}
-          price={product.price}
-          size={product.size}
-        />
-      ));
+ render() {
+  console.log("all products-->", this.state.products);
+  const PRODUCTS = this.state.products
+   .slice(0, 4)
+   .map((product, index) => (
+    <Product
+     key={index}
+     _id={product._id}
+     imageURL={product.imageURL}
+     brand={product.brand}
+     name={product.name}
+     price={product.price}
+     size={product.size}
+    />
+   ));
 
-    return (
-      <>
-        <div className="all-carousels">
-        <div className="buyagain">
-          <div className="product-header">
-            <p className="product-header-title">Buy Again</p>
-            <p className="product-header-vm"><Link to="#">view 57 more</Link></p>
-          </div>
-          <div className="carousel">{PRODUCTS}</div>
-        </div>
+  return (
+   <div className="all-carousels">
+    <div className="buyagain">
+     <div className="product-header">
+      <p className="product-header-title">Buy Again</p>
+      <p className="product-header-vm"><Link to="#">view 57 more</Link></p>
+     </div>
+     <div className="carousel">{PRODUCTS}</div>
+    </div>
+    <div className="buyagain">
+     <div className="product-header">
+      <p className="product-header-title">New Arrivals</p>
+      <p className="product-header-vm"><Link to="#">view 57 more</Link></p>
+     </div>
+     <div className="carousel">{PRODUCTS}</div>
+    </div>
 
-        <div className="buyagain">
-          <div className="product-header">
-            <p className="product-header-title">New Arrivals</p>
-            <p className="product-header-vm"><Link to="#">view 57 more</Link></p>
-          </div>
-          <div className="carousel">{PRODUCTS}</div>
-          </div>
-        </div>
-      </>
-    );
-  }
+   </div>
+
+
+  );
+ }
 }
 
 export default ProductList;
