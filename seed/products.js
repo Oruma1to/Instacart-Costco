77ce3dd67ce7a2a26aa0eb5af2600d124ab71e02
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2020-05-20 14:39:44 -0400

    started new seeds

--- seed/products.js
+++ seed/products.js
@@ -9,52 +9,74 @@ const main = async () => {
     {
       name: "Kirkland Signature Mini Chocolate Chip Cookies",
       brand: "Kirkland",
-      department:"Bakery & Desserts",
+      department: "Bakery & Desserts",
       category: "Cookies",
       imageURL: "https://d2d8wwwkmhfcva.cloudfront.net/600x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_33cf258a-1b53-43fd-9cbb-052a5175edb7.jpeg",
       price: 4.89,
       size: "60ct",
-      quantity:19
+      quantity: 19
     },
 
     {
       name: "Strawberries",
       brand: "Dole",
-      department:"Produce",
+      department: "Produce",
       category: "Fruits",
       imageURL: "https://d2d8wwwkmhfcva.cloudfront.net/600x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_de09de29-a9b9-4d30-89c0-9cf3c669d1df.jpeg",
       price: 4.89,
       size: "2 lbs",
-      quantity:19
+      quantity: 19
     },
 
     {
       name: "Fruit Loops",
       brand: "Kelloggs",
-      department:"Breakfast",
+      department: "Breakfast",
       category: "Cereal",
       imageURL: "https://images.kglobalservices.com/www.kelloggs.com.au/en_au/product/product_4508497/prod_img-3394699_kelloggs-froot-loops.png",
       price: 8.59,
       size: "43.6 oz",
-      quantity:9
+      quantity: 9
     },
-    
+
     {
       name: "Kirkland Signature Organic Grade AA Eggs",
       brand: "Kirkland",
-      department:"Dairy & Eggs",
+      department: "Dairy & Eggs",
       category: "Eggs",
       imageURL: "https://i.imgur.com/15evpwG.jpg",
       price: 7.39,
       size: "24ct",
-      quantity:19
+      quantity: 19
     },
+    {
+      name: "Kirkland Signature USDA Prime Beef Loin Top Sirloin Cap Steak Boneless",
+      brand: "Kirkland",
+      department: "Meat & Seafood",
+      category: "Prime Beef",
+      imageURL: "https://d2d8wwwkmhfcva.cloudfront.net/310x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_0d493cf4-a04a-4afd-8372-ef102f1936d0.jpeg",
+      price: 56.36,
+      size: "4 lbs",
+      quantity: 19
+    },
+    {
+      name: "Kirkland Signature Pork Loin Top Loin Chops",
+      brand: "Kirkland",
+      department: "Meat & Seafood",
+      category: "Pork & Lamb",
+      imageURL: "https://d2d8wwwkmhfcva.cloudfront.net/480x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_12082476-40fc-4b3c-9a16-74cb5fee62e7.jpeg",
+      price: 27.45,
+      size: "6 lbs",
+      quantity: 19
+    },
+
+
   ]
   await Product.insertMany(products)
   console.log("Created Products!")
 }
 
-const run = async() => {
+const run = async () => {
   await main()
   db.close()
 }
