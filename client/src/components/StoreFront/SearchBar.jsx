7f66e9c005ed9b7deecd2b98979a84213b221b61
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-20 19:48:23 -0400

    fixes

--- client/src/components/StoreFront/SearchBar.jsx
+++ client/src/components/StoreFront/SearchBar.jsx
@@ -2,21 +2,38 @@ import React, { Component } from 'react'
 import './SearchBar.css'
 
 export default class SearchBar extends Component {
-
+ constructor() {
+  super();
+  this.state = {
+   products: [],
+   filteredValue: "",
+   filteredPosts: null
+  };
+ }
+ handleOnSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target.value)
+  console.log("I was clicked")
+ }
+ handleSearchChange = e => {
+  console.log(e.target.value)
+  let filteredValue = e.target.value
+  this.setState({ filteredValue })
+ }
  render(props) {
   return (
    <div className="search-bar-container" >
-    <form className="search-bar" onSubmit={(e) => props.onSubmit(e)}>
+    <form className="search-bar" onSubmit={this.handleOnSubmit}>
      <input
       className="search-input"
-      value={props.value}
-      onChange={(e) => props.onChange(e)}
+      value={this.filteredValue}
+      onChange={this.handleSearchChange}
       name="Search"
       placeholder="Search Costco..."
       type="text"
       autoFocus
      />
-     <img className="search-icon" alt="search-icon" src="https://i.imgur.com/xONMjhd.png" />
+     <img className="search-icon" alt="search-icon" src="https://i.imgur.com/xONMjhd.png" onClick={this.handleOnSubmit} />
 
     </form>
 
