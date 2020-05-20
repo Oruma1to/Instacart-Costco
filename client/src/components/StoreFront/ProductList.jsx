1349b4d6f27c00d925aa4e43dc1ffde200f048e4
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-05-20 15:37:43 -0400

    styled view more link responsive

--- client/src/components/StoreFront/ProductList.jsx
+++ client/src/components/StoreFront/ProductList.jsx
@@ -6,54 +6,54 @@ import "./ProductList.css";
 import { Link } from 'react-router-dom'
 
 class ProductList extends Component {
- constructor(props) {
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state = {
-   products: [],
-  };
- }
+    this.state = {
+      products: [],
+    };
+  }
 
- async componentDidMount() {
-  const products = await getProducts();
-  this.setState({ products });
- }
+  async componentDidMount() {
+    const products = await getProducts();
+    this.setState({ products });
+  }
 
- render() {
-  console.log("all products-->", this.state.products);
-  console.log("all products-->", this.props.user);
-  const PRODUCTS = this.state.products
-   // .slice(0, 4)
-   .map((product, index) => (
-    <Product
-     user={this.props.user}
-     key={index}
-     _id={product._id}
-     imageURL={product.imageURL}
-     brand={product.brand}
-     name={product.name}
-     price={product.price}
-     size={product.size}
-    />
-   ));
+  render() {
+    console.log("all products-->", this.state.products);
+    console.log("all products-->", this.props.user);
+    const PRODUCTS = this.state.products
+      // .slice(0, 4)
+      .map((product, index) => (
+        <Product
+          user={this.props.user}
+          key={index}
+          _id={product._id}
+          imageURL={product.imageURL}
+          brand={product.brand}
+          name={product.name}
+          price={product.price}
+          size={product.size}
+        />
+      ));
 
-  return (
-   // <div className="all-carousels"></div>
-   <>
-    <div className="buyagain">
-     <div className="product-header">
-      <p className="product-header-title">Buy Again</p>
-      <p className="product-header-vm"><Link to="#">View 57 more ></Link></p>
-     </div>
-     <div className="carousel">{PRODUCTS}</div>
-    </div>
-   </>
-
-
+    return (
+      // <div className="all-carousels"></div>
+      <>
+        <div className="buyagain">
+          <div className="product-header">
+            <p className="product-header-title">Buy Again</p>
+            <p><Link className="product-header-vm" to="#">View 57 more ></Link></p>
+          </div>
+          <div className="carousel">{PRODUCTS}</div>
+        </div>
+      </>
 
 
-  );
- }
+
+
+    );
+  }
 }
 
 export default ProductList;
