0248075a675b870de57aee261eb264d7eab5f864
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-21 00:26:09 -0400

    refactoer the StoreBody from a function to a class based component

--- client/src/components/StoreFront/StoreBody.jsx
+++ client/src/components/StoreFront/StoreBody.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { DeliveryTo } from "./DeliveryTo";
 import ProductList from "./ProductList";
 import Departments from "./Departments"
 import './StoreBody.css'
 
-export const StoreBody = (props) => {
- console.log("In storeBody", props.user)
- console.log("IN STOREBODY", props.searchProducts)
- return (
-  <div className="storeBodyPage">
-   <DeliveryTo />
-   <main className="storeBodySection">
-    <Departments />
-    <div className="storebody-products-list">
-     <ProductList user={props.user} dataInfo={1} title="Buy Again" />
-     <ProductList user={props.user} dataInfo={2} title="New Arrivals" />
-    </div>
-   </main>
-  </div>
- );
+export default class StoreBody extends Component {
+ constructor(props) {
+  super(props);
+
+  this.state = {
+   products: [],
+  };
+ }
+ render() {
+  console.log("In storeBody", this.props.user)
+  console.log("IN STOREBODY", this.props.searchProducts)
+  return (
+   <div className="storeBodyPage">
+    <DeliveryTo />
+    <main className="storeBodySection">
+     <Departments />
+     {/* {props.searchProducts ? } */}
+     <div className="storebody-products-list">
+      <ProductList user={this.props.user} dataInfo={1} title="Buy Again" />
+      <ProductList user={this.props.user} dataInfo={2} title="New Arrivals" />
+     </div>
+    </main>
+   </div>
+  );
+ }
 };
--- client/src/components/StoreFront/StoreFront.jsx
+++ client/src/components/StoreFront/StoreFront.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import StoreHeader from "./StoreHeader";
-import { StoreBody } from "./StoreBody";
+import StoreBody from "./StoreBody";
 
 import "./StoreFront.css";
 
