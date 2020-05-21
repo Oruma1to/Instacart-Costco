dd6d2b639cf41f86fb0388b1defed3fbff965a01
Abraham E. Tavarez <abetavarez@icloud.com>
2020-05-21 17:58:18 -0400

    Added error checker on signUp jsx

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,81 +1,91 @@
-import React, { Component } from 'react'
-import './SignUp.css'
-import { signUp, signIn } from '../services/user'
-import LandingHeader from './LandingHeader'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import "./SignUp.css";
+import { signUp, signIn } from "../services/user";
+import LandingHeader from "./LandingHeader";
+import { Link } from "react-router-dom";
 
 class CreateAccount extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      username: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
       isError: false,
-      errorMsg: ''
-    }
+      errorMsg: "",
+    };
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
       isError: false,
-      errorMsg: ''
-    })
-  }
+      errorMsg: "",
+    });
+  };
 
-  onSignUp = event => {
-    event.preventDefault()
+  onSignUp = (event) => {
+    event.preventDefault();
 
-    const { history, setUser } = this.props
-    console.log(this.state)
+    const { history, setUser } = this.props;
+    console.log(this.state);
     signUp(this.state)
       .then(() => signIn(this.state))
-      .then(res => setUser(res.user))
-      .then(() => history.push('/costco'))
-      .catch(error => {
-        console.error(error)
+      .then((res) => setUser(res.user))
+      .then(() => history.push("/costco"))
+      .catch((error) => {
+        console.error(error);
+
+        let errorMSG = error ? validationError : "nothing here";
+
         this.setState({
-          username: '',
-          email: '',
-          password: '',
+          username: "",
+          email: "",
+          password: "",
           isError: true,
-          errorMsg: 'Sign Up Details Invalid'
-        })
-      })
-  }
+          errorMsg: errorMSG,
+        });
+      });
+  };
 
   renderError = () => {
-    const toggleForm = this.state.isError ? 'danger-signup' : ''
+    const toggleForm = this.state.isError ? "danger-signup" : "";
     if (this.state.isError) {
       return (
         <button type="submit" className={toggleForm}>
           {this.state.errorMsg}
         </button>
-      )
+      );
     } else {
-      return <button className="login-signup" type="submit">Sign Up & Log In</button>
+      return (
+        <button className="login-signup" type="submit">
+          Sign Up & Log In
+        </button>
+      );
     }
-  }
+  };
 
   render() {
-    const { username, email, password } = this.state
+    const { username, email, password } = this.state;
     return (
-
       <>
         <LandingHeader />
 
         <div className="form-container-signup">
-          <img className="sign-in-instacart-detail-image" alt="instacart-logo"
-            src={require('../images/instacartlogo@3x.png')} />
+          <img
+            className="sign-in-instacart-detail-image"
+            alt="instacart-logo"
+            src={require("../images/instacartlogo@3x.png")}
+          />
 
           {/* <div className="instacart-detail-image"></div> */}
           <div className="signup-welcome">Welcome to Instacart</div>
-          <div className="signup-prompt">Create a profile with an email and password</div>
+          <div className="signup-prompt">
+            Create a profile with an email and password
+          </div>
           <form onSubmit={this.onSignUp}>
-
             <div className="usernameInput">
               <input
                 required
@@ -112,24 +122,30 @@ class CreateAccount extends Component {
             </div>
             {this.renderError()}
           </form>
-          
+
           <div className="separator-signup">or</div>
 
           <div className="orLoginInfo">
             <div>
-              <button className="facebook-button-signup">Sign up with Facebook</button>
+              <button className="facebook-button-signup">
+                Sign up with Facebook
+              </button>
             </div>
             <div>
-              <button className="google-button-signup">Sign up with Google</button>
+              <button className="google-button-signup">
+                Sign up with Google
+              </button>
             </div>
             <div className="already-signup">
-              <h4>Already have an account? <Link to="/sign-in">Log In</Link></h4>
+              <h4>
+                Already have an account? <Link to="/sign-in">Log In</Link>
+              </h4>
             </div>
           </div>
-        </div >
+        </div>
       </>
-    )
+    );
   }
 }
 
-export default CreateAccount
+export default CreateAccount;
