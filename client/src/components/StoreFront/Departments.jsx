91153432722a18427a10c5b599d1956ef6311cf2
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-21 18:10:23 -0400

    finish with the category links

--- client/src/components/StoreFront/Departments.jsx
+++ client/src/components/StoreFront/Departments.jsx
@@ -18,17 +18,17 @@ export default class Departments extends Component {
 
        <ul className="parent-sub-menu">
 
-        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar("Prime Beef")}>Prime Beef</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Prime Beef")}>Prime Beef</Link></li>
 
-        <li><Link className="department-item" to="#">Pork & Lamb</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Pork & Lamb")}>Pork & Lamb</Link></li>
 
-        <li><Link className="department-item" to="#">Beef</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Beef")}>Beef</Link></li>
 
-        <li><Link className="department-item" to="#">Seafood</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Seafood")}>Seafood</Link></li>
 
-        <li><Link className="department-item" to="#">Poultry</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Poultry")}>Poultry</Link></li>
 
-        <li><Link className="department-item" to="#">Meat</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Meat")}>Meat</Link></li>
 
        </ul>
 
@@ -38,13 +38,13 @@ export default class Departments extends Component {
 
        <ul className="parent-sub-menu">
 
-        <li><Link className="department-item" to="#">Fruit & Vegetable Trays</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Fruit & Vegetable Trays")}>Fruit & Vegetable Trays</Link></li>
 
-        <li><Link className="department-item" to="#">Fruit</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Fruit")}>Fruit</Link></li>
 
-        <li><Link className="department-item" to="#">Vegetables</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Vegetables")}>Vegetables</Link></li>
 
-        <li><Link className="department-item" to="#">Salad Mix</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Salad Mix")}>Salad Mix</Link></li>
 
        </ul></li>
 
@@ -52,26 +52,26 @@ export default class Departments extends Component {
 
       <ul className="parent-sub-menu">
 
-       <li><Link className="department-item" to="#">Meat & Cheese Trays</Link></li>
+       <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Meat & Cheese Trays")}>Meat & Cheese Trays</Link></li>
 
-       <li><Link className="department-item" to="#">Prepared Meals</Link></li>
+       <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Prepared Meals")}>Prepared Meals</Link></li>
 
-       <li><Link className="department-item" to="#">Prepared Soups & Salad</Link></li>
+       <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Prepared Soups & Salad")}>Prepared Soups & Salad</Link></li>
 
-       <li><Link className="department-item" to="#">Dips & Spreads</Link></li>
+       <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Dips & Spreads")}>Dips & Spreads</Link></li>
       </ul>
 
       <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('department', "Bakery & Desserts")}>Bakery & Desserts</Link>
 
        <ul className="parent-sub-menu">
 
-        <li><Link className="department-item" to="#">Bread</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Bread")}>Bread</Link></li>
 
-        <li><Link className="department-item" to="#">Buns & Rolls</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Buns & Rolls")}>Buns & Rolls</Link></li>
 
-        <li><Link className="department-item" to="#">Cookies</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Cookies")}>Cookies</Link></li>
 
-        <li><Link className="department-item" to="#">Pies & Cakes</Link></li>
+        <li><Link className="department-item" to="#" onClick={() => this.props.linkNavBar('category', "Pies & Cakes")}>Pies & Cakes</Link></li>
 
        </ul>
 
