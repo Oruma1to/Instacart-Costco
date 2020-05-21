6e4673c8bfa68d0a9858e0967126ea17b4cb787c
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-21 00:39:43 -0400

    Was able to have functionailty work for search and rendering to the body

--- client/src/components/StoreFront/StoreBody.jsx
+++ client/src/components/StoreFront/StoreBody.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { DeliveryTo } from "./DeliveryTo";
 import ProductList from "./ProductList";
 import Departments from "./Departments"
+import Product from "../StoreFront/Product"
 import './StoreBody.css'
 
 export default class StoreBody extends Component {
@@ -9,22 +10,38 @@ export default class StoreBody extends Component {
   super(props);
 
   this.state = {
-   products: [],
+   foundProducts: [],
   };
  }
  render() {
   console.log("In storeBody", this.props.user)
   console.log("IN STOREBODY", this.props.searchProducts)
+  const products =
+   this.props.searchProducts ?
+    this.props.searchProducts.map((product, index) => (
+     <Product
+      user={this.props.user}
+      key={index}
+      _id={product._id}
+      imageURL={product.imageURL}
+      brand={product.brand}
+      name={product.name}
+      price={product.price}
+      size={product.size}
+     />
+    )) :
+    "";
   return (
    <div className="storeBodyPage">
     <DeliveryTo />
     <main className="storeBodySection">
      <Departments />
-     {/* {props.searchProducts ? } */}
-     <div className="storebody-products-list">
-      <ProductList user={this.props.user} dataInfo={1} title="Buy Again" />
-      <ProductList user={this.props.user} dataInfo={2} title="New Arrivals" />
-     </div>
+     {products ? <div className="carousel">{products}</div> :
+      <div className="storebody-products-list">
+       <ProductList user={this.props.user} dataInfo={1} title="Buy Again" />
+       <ProductList user={this.props.user} dataInfo={2} title="New Arrivals" />
+      </div>
+     }
     </main>
    </div>
   );
