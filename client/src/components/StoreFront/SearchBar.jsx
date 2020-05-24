eb21ca4d93721796f10641b121060e28e75804e2
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-24 19:35:01 -0400

    Here when the user clicks away it just erase anything on the search mainly .. for when the users clicks the department links

--- client/src/components/StoreFront/SearchBar.jsx
+++ client/src/components/StoreFront/SearchBar.jsx
@@ -4,50 +4,55 @@ import { getSearchProducts } from "../../services/product"
 import './SearchBar.css'
 
 export default class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filteredValue: ""
-    };
+ constructor(props) {
+  super(props);
+  this.state = {
+   filteredValue: ""
+  };
+ }
+ search = async () => {
+  const { filteredValue } = this.state
+  let searchProducts = await getSearchProducts(filteredValue);
+  this.props.setSearchProducts(searchProducts)
+ }
+
+ handleOnSubmit = (e) => {
+  e.preventDefault();
+ }
+
+ handleClickAway = (e) => {
+  console.log("in the clickaway", e.target.value)
+  e.target.value = "";
+ }
+
+
+ handleSearchChange = e => {
+  let filteredValue = e.target.value
+  if (filteredValue !== "") {
+   this.setState({ filteredValue }, this.search)
+  } else {
+   this.props.setSearchProducts(null)
   }
-  search = async () => {
-    const { filteredValue } = this.state
-    let searchProducts = await getSearchProducts(filteredValue);
-    this.props.setSearchProducts(searchProducts)
-  }
+ }
 
-  handleOnSubmit = (e) => {
-    e.preventDefault();
-  }
+ render(props) {
+  return (
+   <div className="search-bar-container" >
+    <form className="search-bar" onSubmit={this.handleOnSubmit} onBlur={this.handleClickAway}>
+     <input
+      className="search-input"
+      value={this.filteredValue}
+      onChange={this.handleSearchChange}
+      name="Search"
+      placeholder="Search Costco..."
+      type="text"
+      autoFocus
+     />
+     <img className="search-icon" alt="search-icon" src="https://i.imgur.com/xONMjhd.png" onClick={this.handleOnSubmit} />
 
+    </form>
 
-  handleSearchChange = e => {
-    let filteredValue = e.target.value
-    if (filteredValue !== "") {
-      this.setState({ filteredValue }, this.search)
-    } else {
-      this.props.setSearchProducts(null)
-    }
-  }
-
-  render(props) {
-    return (
-      <div className="search-bar-container" >
-        <form className="search-bar" onSubmit={this.handleOnSubmit}>
-          <input
-            className="search-input"
-            value={this.filteredValue}
-            onChange={this.handleSearchChange}
-            name="Search"
-            placeholder="Search Costco..."
-            type="text"
-            autoFocus
-          />
-          <img className="search-icon" alt="search-icon" src="https://i.imgur.com/xONMjhd.png" onClick={this.handleOnSubmit} />
-
-        </form>
-
-      </div>
-    )
-  }
+   </div>
+  )
+ }
 }
