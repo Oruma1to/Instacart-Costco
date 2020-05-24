78f18176497442f3ad00fff889f43995b6b77a44
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-24 15:56:59 -0400

    changing the async getProducts from the StoreBody component to StoreFront compoenet

--- client/src/components/StoreFront/StoreFront.jsx
+++ client/src/components/StoreFront/StoreFront.jsx
@@ -5,12 +5,23 @@ import StoreBody from "./StoreBody";
 import "./StoreFront.css";
 
 export default class StoreFront extends Component {
-  render() {
-    return (
-      <div className="store-front">
-        <StoreHeader user={this.props.user} setSearchProducts={this.props.setSearchProducts} />
-        <StoreBody user={this.props.user} searchProducts={this.props.searchProducts} />
-      </div>
-    );
+ constructor() {
+  super()
+  this.state = {
+   products: null
   }
+ }
+ async componentDidMount() {
+  const products = await getProducts();
+  this.setState({ products })
+ }
+
+ render() {
+  return (
+   <div className="store-front">
+    <StoreHeader user={this.props.user} setSearchProducts={this.props.setSearchProducts} />
+    <StoreBody user={this.props.user} searchProducts={this.props.searchProducts} />
+   </div>
+  );
+ }
 }
